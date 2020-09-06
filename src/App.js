d5401df1b7c5a5f74d8395f9d1f6da6b670b6e1f
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-09-07 01:08:52 +0300

    installing react-router and linking navbar icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import Navbar from './containers/Navbar';
-
+import HomePage from './containers/HomePage';
+import SkillsPage from './containers/SkillsPage';
+import ContactPage from './containers/ContactPage';
+import AboutPage from './containers/AboutPage';
+import { Switch, Route } from 'react-router-dom';
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/skills" component={SkillsPage} />
+        <Route path="/about" component={AboutPage} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
