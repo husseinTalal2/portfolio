9c9704c03648fa463e155d3c2d39ef6d3f0a5427
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-09-06 21:45:53 +0300

    adding the main responsive navbar structure

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -8,24 +8,36 @@ import github from '../imgs/GitHub.svg';
 import linkedin from '../imgs/Linkedin.svg';
 function Navbar() {
   return (
-    <div className="navbar">
-      <nav className="h-screen w-16 bg-gray-900 flex flex-col justify-between">
-        <div className="flex flex-col justify-center items-center my-5 text-4xl">
-          <span className="logo text-yellow-500">H</span>
-        </div>
-        <div className="nav-items flex flex-col">
-          <img className="my-5" src={home} alt="home icon" />
-          <img className="my-5" src={about} alt="about icon" />
-          <img className="my-5" src={skills} alt="skills icon" />
-          <img className="my-5" src={contact} alt="contact icon" />
-        </div>
-        <div className="social-icons flex flex-col my-5">
-          <img className="my-3" src={github} alt="github icon" />
-          <img className="my-3" src={linkedin} alt="linkedin icon" />
-          <img className="my-3" src={instagram} alt="instagram icon" />
-        </div>
-      </nav>
-    </div>
+    <>
+      <div className="navbar hidden sm:block ">
+        <nav className="h-screen w-16 bg-darker flex flex-col justify-between">
+          <div className="flex flex-col justify-center items-center my-5 text-4xl">
+            <span className="logo text-yellow-500">H</span>
+          </div>
+          <div className="nav-items flex flex-col">
+            <img className="my-5" src={home} alt="home icon" />
+            <img className="my-5" src={about} alt="about icon" />
+            <img className="my-5" src={skills} alt="skills icon" />
+            <img className="my-5" src={contact} alt="contact icon" />
+          </div>
+          <div className="social-icons flex flex-col my-5">
+            <img className="my-3" src={github} alt="github icon" />
+            <img className="my-3" src={linkedin} alt="linkedin icon" />
+            <img className="my-3" src={instagram} alt="instagram icon" />
+          </div>
+        </nav>
+      </div>
+      <div className="mobile-nav sm:hidden absolute bottom-0">
+        <nav className="h-12 bg-dark flex items-center">
+          <div className="flex justify-evenly items-center w-screen">
+            <img src={home} alt="home icon" />
+            <img src={about} alt="about icon" />
+            <img src={skills} alt="skills icon" />
+            <img src={contact} alt="contact icon" />
+          </div>
+        </nav>
+      </div>
+    </>
   );
 }
 
