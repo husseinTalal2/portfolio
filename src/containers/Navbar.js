d3b2b46383bf362694b57bf50dd23b5adbba428e
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-09-06 21:56:06 +0300

    adding social media links

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
       <div className="navbar hidden sm:block ">
         <nav className="h-screen w-16 bg-darker flex flex-col justify-between">
           <div className="flex flex-col justify-center items-center my-5 text-4xl">
-            <span className="logo text-yellow-500">H</span>
+            <span className="logo text-primaryYellow">H</span>
           </div>
           <div className="nav-items flex flex-col">
             <img className="my-5" src={home} alt="home icon" />
@@ -20,10 +20,28 @@ function Navbar() {
             <img className="my-5" src={skills} alt="skills icon" />
             <img className="my-5" src={contact} alt="contact icon" />
           </div>
-          <div className="social-icons flex flex-col my-5">
-            <img className="my-3" src={github} alt="github icon" />
-            <img className="my-3" src={linkedin} alt="linkedin icon" />
-            <img className="my-3" src={instagram} alt="instagram icon" />
+          <div className="social-icons flex items-center flex-col my-5">
+            <a
+              className="my-3"
+              href="https://github.com/husseinTalal2"
+              target="blank"
+            >
+              <img src={github} alt="github icon" />
+            </a>
+            <a
+              className="my-3"
+              href="https://www.linkedin.com/in/hussein-talal-5b77a81a7/"
+              target="blank"
+            >
+              <img src={linkedin} alt="linkedin icon" />
+            </a>
+            <a
+              className="my-3"
+              href="https://www.instagram.com/hossien_talal/"
+              target="blank"
+            >
+              <img src={instagram} alt="instagram icon" />
+            </a>
           </div>
         </nav>
       </div>
