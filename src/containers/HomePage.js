ed3ae6b83a1036ca11480f1d553d64b082d2020e
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-09-07 02:28:32 +0300

    adding alt to the imgs

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -5,7 +5,11 @@ function HomePage() {
   return (
     <div className="flex flex-col my-5 mx-auto items-center justify-center ">
       <h1 className="text-4xl">UNDER CONSTRUCTION</h1>
-      <img className="w-screen lg:w-1/2" src={underConstruction} />
+      <img
+        className="w-screen lg:w-1/2"
+        src={underConstruction}
+        alt="under construction"
+      />
     </div>
   );
 }
