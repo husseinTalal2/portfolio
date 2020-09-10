57a0500242b899f9bf2f55ea825e5bbc7cc997ee
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-09-10 23:56:01 +0300

    adding about page

---
+++ src/containers/AboutPage/AboutPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AboutMe from '../../components/ِAboutPage/AboutMe';
+import Certificates from '../../components/ِAboutPage/Certificates';
+import CV from '../../components/ِAboutPage/CV';
+import Testimonials from '../../components/ِAboutPage/Testimonials';
+
+function AboutPage() {
+  return (
+    <>
+      <AboutMe />
+      <Certificates />
+      <Testimonials />
+      <CV />
+    </>
+  );
+}
+
+export default AboutPage;
