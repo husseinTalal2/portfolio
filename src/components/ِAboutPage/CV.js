c27986549920af9de9fdecc840f8275866194d16
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-09-11 00:49:29 +0300

    fixing lint problems

--- src/components/ِAboutPage/CV.js
+++ src/components/ِAboutPage/CV.js
@@ -7,7 +7,9 @@ function CV() {
         Download my CV
       </h1>
       <div className="flex flex-col mt-16 justify-center items-center text-white">
-        <p className="text-justify px-10">Download my full CV to know all the details about me in one page </p>
+        <p className="text-justify px-10">
+          Download my full CV to know all the details about me in one page{' '}
+        </p>
         <button className="button text-center my-12 bg-transparent w-32 text-base hover:bg-primaryYellow text-primaryYellow font-normal hover:text-darker py-2 px-4 border border-primaryYellow hover:border-transparent rounded-none">
           Download
         </button>
