e639ecd96d89846af262aab2d1a3e186b3fa5617
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-09-10 23:56:15 +0300

    adding about page sections

---
+++ src/components/ِAboutPage/AboutMe.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <div className="bg-dark flex w-screen h-full lg:h-screen">
+      <div className="lg:px-24 xs:px-4 mt-24 md:mt-40 lg:w-1/2">
+        <div className=" lg:mx-0 mx-12 lg:mt-24 lg:block flex flex-col items-center">
+          <h1 className="inline text-black bg-primaryYellow text-3xl lg:text-5xl font-semibold">
+            About me
+          </h1>
+          <p className="my-5 text-lightGray text-justify">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+            auctor sem purus, vitae finibus arcu maximus in. Aliquam erat
+            volutpat. Mauris bibendum dictum malesuada. Mauris pellentesque
+            dignissim urna, vel molestie ex convallis vitae. Donec porttitor
+            tristique euilgod. Vestibulum quis lectus et sem efficitur finibus
+            vel eu enim
+          </p>
+        </div>
+      </div>
+      <div className="lg:w-1/2 bg-primaryYellow text-4xl hidden lg:flex items-center justify-center">
+        <h1
+          className="text-4xl"
+          style={{ fontSize: '20rem', fontWeight: 'bold' }}
+        >{`</>`}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
---
+++ src/components/ِAboutPage/CV.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function CV() {
+  return (
+    <div className="w-screen flex flex-col justify-center items-center bg-dark my-48">
+      <h1 className="inline text-black bg-primaryYellow lg:text-5xl text-3xl font-semibold">
+        Download my CV
+      </h1>
+      <div className="flex flex-col mt-16 justify-center items-center text-white">
+        <p className="text-justify px-10">Download my full CV to know all the details about me in one page </p>
+        <button className="button text-center my-12 bg-transparent w-32 text-base hover:bg-primaryYellow text-primaryYellow font-normal hover:text-darker py-2 px-4 border border-primaryYellow hover:border-transparent rounded-none">
+          Download
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CV;
