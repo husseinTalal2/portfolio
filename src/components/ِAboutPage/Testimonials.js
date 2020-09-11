5626b924bb0b49e29ee16384af6ed6fc29574b41
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-09-11 11:25:02 +0300

    adding more screen breck points for responsivnes

--- src/components/ِAboutPage/Testimonials.js
+++ src/components/ِAboutPage/Testimonials.js
@@ -6,13 +6,13 @@ function Testimonials() {
       <h1 className="inline text-black bg-primaryYellow lg:text-5xl text-3xl font-semibold">
         Testimonials
       </h1>
-      <div className="md:w-3/4 w-4/5  mt-16 justify-center bg-inputColor items-center text-white rounded">
-        <p className="text-justify text-2xl mb-16 mt-12 mx-12">
+      <div className="md:w-3/4 w-4/5  lg:mt-16 mt-16 justify-center bg-inputColor items-center text-white rounded">
+        <p className="text-justify lg:text-2xl text-base lg:mb-16 mb-8 lg:mt-12 mt-8 lg:mx-12 mx-6">
           {'"'}What I like about Hussein is that he is calm, his pieces of
           information are always top-notch, he cares about everyone and offers
           his help all the time.{'"'}
         </p>
-        <p className="mb-8 ml-12 md:text-base text-xs">
+        <p className="mb-8 lg:ml-12 ml-6 md:text-base text-xs">
           Osama - Instructor at Re:Coded
         </p>
       </div>
